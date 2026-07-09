6ef8a234c7ac04b98c6592b68207ba6a640a04da
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 17:51:20 +0530

    Implement basic arithmetic operations in C
    Fix variable assignments and print statements for sum, subtraction, multiplication, and division.

---
+++ Add sun mul div.c
@@ -0,0 +1,20 @@
+//Shrushti parte
+//2503033111378UD011
+ #include<stdio.h>
+ int main()
+{
+int num1, num2, sum, sub, mul, div;
+ printf("Enter first number");
+scanf("%d,&num1);
+printf("Enter second number");
+ scanf("%d,&num2);
+ add=num1+num2;
+ printf("sum is %d", & sum);
+sub=num1-num2;
+printf("sum is %d", & sub);
+mul=num1*num2;
+printf("sum is %d", & mul);
+div=num1/num2;
+printf("sum is %d", & div);
+return 0; 
+ }
