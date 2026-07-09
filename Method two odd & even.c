24004f74abf309485fccf91d5d24194d00ce0bb8
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 19:18:54 +0530

    Create Method two odd & even.c

---
+++ Method two odd & even.c
@@ -0,0 +1,15 @@
+//Name-Shrushti Sandip Parte 
+//PRN- 251303211137800011 
+#include <stdio.h>
+ int main()
+{
+int num;
+printf("Enter a number:");
+scanf("%d", &num);
+}
+if (num%2==0)
+printf("Even Number");
+else
+printf("Odd Number");
+return 0;
+}
