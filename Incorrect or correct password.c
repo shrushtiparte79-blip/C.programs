f7006f199f2c11305ba7a77fd46ad1342bf1849e
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 19:28:55 +0530

    Create Incorrect or correct password.c

---
+++ Incorrect or correct password.c
@@ -0,0 +1,14 @@
+//Name-Shrushti Sandip Parte
+// PRN-2503033111378UD011
+ #include <stdio.h>
+ int main()
+{
+int pin;
+printf("Enter ATM PIN: ");
+scanf("%d", &pin);
+if(pin == 7523)
+printf("Access Granted");
+else
+printf("Wrong PIN");
+return 0;
+}
