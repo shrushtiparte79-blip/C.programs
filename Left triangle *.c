3993298a490554d354e3809e7de7756fcba9dc54
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 18:58:57 +0530

    Create Left triangle *.c

---
+++ Left triangle *.c
@@ -0,0 +1,12 @@
+// Name: Shrushti Sandip Parte
+//PRN: 2503033111378UD011
+ #include<stdio.h>
+ int main () {
+int i = 1,n, fact= 1;
+printf("Enter number: ");
+scanf("%d",&n);
+for (i=1; i<=n; i++) {
+fact=fact*i;
+printf("Factorial: \% d", fact);
+return 0;
+}
