f67eb494d3c2fbaa299dd4fd23a268b809cfa78a
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 19:06:12 +0530

    Create Left upside triangle *.c

---
+++ Left upside triangle *.c
@@ -0,0 +1,14 @@
+//Name:-Shrushti Sandip Parte 
+//PRN-2503033111378UD011 
+#include<stdio.h>
+ int main(){
+int i,j;
+for(i=1;i<=3;i++){
+}
+for(j=1;j<=3;j++){
+printf("*\t");
+ }
+printf("\n");
+}
+return 0;
+}
