85b4494580d51a7e499d15ec39bb0fda080fd10c
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 18:45:23 +0530

    Implement grading logic based on marks input
    Fix syntax errors in the grading logic and improve formatting.

---
+++ Grade or Fail.c
@@ -0,0 +1,21 @@
+//Name-Shrushti Sandip Parte
+//PRN-2503033111378UD011
+#include <stdio.h>
+int main()
+{
+int marks;
+printf("Enter Marks: ");
+scanf("%d", &marks);
+if{(marks >= 90)
+printf("Grade A");
+else if(marks >= 75)
+printf("Grade B");
+else if (marks >= 60)
+printf("Grade C");
+else if(marks >= 40)
+printf("Grade D");
+else
+printf("Fail");
+}
+return 0;
+}
