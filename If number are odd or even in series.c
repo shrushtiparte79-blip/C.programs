f32f0dd5cd22bf1aad193398f7f564dfaee555f9
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 19:22:35 +0530

    Create If number are odd or even in series.c

---
+++ If number are odd or even in series.c
@@ -0,0 +1,27 @@
+//PRN-2503033111376UD011
+//Name Shrushti Sandip Parte
+#include <stdio.h>
+int main() {
+int a[10],b[10].even [10],odd [10],e=0,0-0,1;
+for (1=0; i<10; 1++) {
+}
+printf ("Enter value at index %d",1);
+scanf("%d",a[i]);
+for(i=0; i<10; 1++) {
+if(a[i]%2==0) {
+even[e]=a[1];
+e++;
+else {
+odd[o]=a[i];
+0++;
+("\nΕΝΕΝ Νο:");
+for(i=0; i<e; i++);
+("%d\t",even[1]);
+}
+("\nODD No:");
+for(i=0; i<0; i++);
+{
+("%d\t", odd[i]);
+}
+return 0;
+}
