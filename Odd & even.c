1bc102f470583c5828988b7f4bb401ad6623c57e
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 18:34:20 +0530

    Create Odd & even.c

---
+++ Odd & even.c
@@ -0,0 +1,14 @@
+//Name-Shrushti Sandip Parte
+// PRN- 2503033111378UD011
+#include <stdio.h>
+int main()
+{
+int num;
+printf("Enter a number:");
+scanf("%d", &num);
+if(num%2==0)
+printf("Even Number");
+else
+printf("Odd Number");
+return 0;
+}
