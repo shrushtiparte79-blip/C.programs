91dfbbc41f29478540b12a67c4b5c8bdb3ac5593
shrushtiparte79-blip <shrushtiparte79@gmail.com>
2026-07-09 19:16:19 +0530

    Create Enter the number.c

---
+++ Enter the number.c
@@ -0,0 +1,21 @@
+//Name-Shrushti Sandip Parte
+//PRN-2503033111378UD011 
+ #include <stdio.h>
+int main()
+{
+int i, j, n;
+printf("Enter number of rows:\n");
+scanf("%d", &n);
+{
+for(i= n; i >= 1: 1--)
+for(j=1;j<=n 1; j++)
+printf(" ");
+{
+for(j=1; j <= 1; j++)
+{
+printf("*");
+}
+printf("\n");
+}
+return 0;
+}
